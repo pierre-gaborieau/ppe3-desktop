77dee6df03cdc80aeb3d5da8703adc270112e9a8
laugere <martin@justmagic.com>
2019-10-01 13:58:34 +0200

    modifierFIlm

---
+++ ppe3-desktop/VUES/COMPOSANT/FILM/ModifierFilm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ppe3_desktop
+{
+    public partial class ModifierFilm : UserControl
+    {
+        public ModifierFilm()
+        {
+            InitializeComponent();
+        }
+    }
+}

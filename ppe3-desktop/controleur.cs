8f1aa2aee9950437e0ad6c3a16e2954a38b881a0
pierre-gaborieau <pierre.gaborieau85@gmail.com>
2019-10-01 16:02:13 +0200

    validation client

--- ppe3-desktop/controleur.cs
+++ ppe3-desktop/controleur.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Linq;
 
 using ppe3_desktop.VUES.COMPTE;
 using ppe3_desktop.VUES.COMPOSANT;
@@ -27,14 +26,9 @@ namespace ppe3_desktop
         private void ValidationComtpeToolStripMenuItem_Click(object sender, EventArgs e)
         {
             allFalse();
-            List<client> listeSend = new List<client>();
-            foreach(client c in maConnexion.client.ToList())
-            {
-                if(c.actif == 0)
-                {
-                    listeSend.Add(c);
-                }
-            }
+
+            var listeSend = maConnexion.client.SqlQuery("SELECT * FROM client WHERE actif = 0").ToList();
+
             validationCompte1.afficherPage(listeSend);
 
             validationCompte1.Visible = true;
@@ -45,10 +39,10 @@ namespace ppe3_desktop
 
             using (var context = new connexionBase())
             {
-                /*int query = context.client
-                    .ExecuteSqlCommand("UPDATE actif in CLIENT SET 1 where login=@login", new SqlParameter("@login", c.login));
-                Console.WriteLine(query.nomClient);*/
+                int noOfRowUpdated = context.Database.ExecuteSqlCommand("Update client set actif=1 where idClient = @id", new SqlParameter("@id", c.idClient));
             }
+
+            allFalse();
 
         }
 

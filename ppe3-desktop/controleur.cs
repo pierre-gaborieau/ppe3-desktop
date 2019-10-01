daad6170cfa2153e2a1197affa545e129fccd754
pierre-gaborieau <pierre.gaborieau85@gmail.com>
2019-10-01 11:17:08 +0200

    Ajoutez des fichiers projet.

---
+++ ppe3-desktop/controleur.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ppe3_desktop
+{
+    public partial class controleur : Form
+    {
+        connexionBase maConnexion;
+        public controleur()
+        {
+            InitializeComponent();
+            maConnexion = new connexionBase();
+        }
+    }
+}
